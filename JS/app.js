423e3ef8c5fb8a12f50e3a30127375047dba1f97
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-12-28 19:07:28 +0100

    ADD: icon and number to each square in the grid, first manually then dinamically

--- JS/app.js
+++ JS/app.js
@@ -2,13 +2,26 @@
 const gridContainer = document.querySelector(".grid-container");
 
 //per ogni elemento in array source
-source.forEach(el => {
+source.forEach((el, i) => {
     //creo un div
     const gridElement = document.createElement("div");
-    //con classe grid-item
+    //con classe grid-item e num
     gridElement.classList.add("grid-item")
+    gridElement.classList.add("num")
 
-    //e lo metto in grid container
+    //definisco l'icona e numero da metterci dentro
+    let icon = el.icon;
+    let num = i + 1;
+
+    //definisco l'elemento img come stringa che ha la variabile icon nel src
+    const imgString = `<img class="icon" src="images/icons/${icon}.png" />`
+    //aggiungo elemento img dentro al grid element
+    gridElement.innerHTML += imgString;
+
+    //aggiungo numero dentro al grid element
+    gridElement.innerHTML += num;
+
+    //metto grid element nel grid container
     gridContainer.append(gridElement);
 })
 
