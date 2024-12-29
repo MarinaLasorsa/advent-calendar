f0ab870200dd86d8521720bd159c373d32ee68bc
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-12-29 19:01:52 +0100

    ADD: js functions to create modal content dynamically

--- JS/app.js
+++ JS/app.js
@@ -26,9 +26,49 @@ source.forEach((el, i) => {
 
     //quando grid element viene cliccato
     gridElement.addEventListener("click", function () {
+
+        //crea un overlay element
+        const overlayElement = document.createElement("div");
+        //gli da' la classe overlay
+        overlayElement.classList.add("overlay")
+        //lo aggiunge a grid element
+        gridElement.prepend(overlayElement);
+
+        //toglie sfondo a grid element
+        gridElement.style.backgroundColor = "transparent";
+
         //definisco modale
-        const modal = document.querySelector(".modal");
-        //cambio il suo display status in flex
+        const modal = document.querySelector(".modal-bg");
+        //definisco container per content
+        const modalContent = document.querySelector(".modal-content");
+
+        //ripulisco il contenuto
+        modalContent.innerHTML = "";
+
+        //se il type dell'array element è image
+        if (el.type === "image") {
+            //creo un elemento img
+            const modalImage = document.createElement("img");
+            //gli assegno l'url dell' array element
+            modalImage.src = el.url;
+            //gli aggiungo la classe modal-image
+            modalImage.classList.add("modal-image");
+            //metto elemento img in modale
+            modalContent.prepend(modalImage);
+
+            //invece se il type dell'array element è text
+        } else if (el.type === "text") {
+            //creo un elemento p
+            const modalText = document.createElement("p");
+            //gli assegno il testo dell'array element
+            modalText.textContent = el.text;
+            //gli aggiungo la classe modal-text
+            modalText.classList.add("modal-text");
+            //metto elemento paragrafo in modale
+            modalContent.prepend(modalText);
+        }
+
+        //cambio il display status di modal in flex
         modal.style.display = "flex"
 
         //definisco bottone chiudi
--- JS/source.js
+++ JS/source.js
@@ -22,7 +22,7 @@ const source = [
     {
         "type": "text",
         "icon": "ico-omino",
-        "text": "La stella di Natale sembra una pianta tipicamente invernale, invece nasce come cespuglio nel caldo del Messico ed era conosciuta fin dai tempi degli Aztechi"
+        "text": "La stella di Natale sembra una pianta tipicamente invernale, invece nasce come cespuglio nel caldo del Messico ed era conosciuta fin dai tempi degli Aztechi."
     },
     {
         "type": "image",
@@ -32,7 +32,7 @@ const source = [
     {
         "type": "text",
         "icon": "ico-pupazzo",
-        "text": "Babbo Natale non ha sempre indossato abiti rossi!Lo sapevi che anni fa indossava il verde?"
+        "text": "Babbo Natale non ha sempre indossato abiti rossi! Lo sapevi che anni fa indossava il verde?"
     },
     {
         "type": "text",
@@ -47,7 +47,7 @@ const source = [
     {
         "type": "text",
         "icon": "ico-pupazzo",
-        "text": "Natale a Luglio? A Cuba è stato così per quasi 30 anni, fra il 1968 e il 1997: i festeggiamenti avvenivano in piena estate"
+        "text": "Natale a Luglio? A Cuba è stato così per quasi 30 anni, fra il 1968 e il 1997: i festeggiamenti avvenivano in piena estate."
     },
     {
         "type": "text",
@@ -62,7 +62,7 @@ const source = [
     {
         "type": "text",
         "icon": "ico-calza",
-        "text": "Pensi di essere un campione seriale di regali? Beh, i francesi hanno regalato agli americani la Statua della Libertà in segno di amicizia nel Natale del 1883"
+        "text": "Pensi di essere un campione seriale di regali? Beh, i francesi hanno regalato agli americani la Statua della Libertà in segno di amicizia nel Natale del 1883."
     },
     {
         "type": "image",
@@ -72,7 +72,7 @@ const source = [
     {
         "type": "text",
         "icon": "ico-stella",
-        "text": "In Svezia c'è una versione local di Babbo Natale: si chiama Tomte, è basso come un folletto e gira accompagnato da Yule, la sua capra"
+        "text": "In Svezia c'è una versione local di Babbo Natale: si chiama Tomte, è basso come un folletto e gira accompagnato da Yule, la sua capra."
     },
     {
         "type": "image",
@@ -82,22 +82,22 @@ const source = [
     {
         "type": "text",
         "icon": "ico-caramella",
-        "text": "Anche tu addobbi l'albero solo da un lato?Chissà se lo fecero anche gli antichi Egizi quando nel loro equivalente del Natale addobbavano le Piramidi"
+        "text": "Anche tu addobbi l'albero solo da un lato?Chissà se lo fecero anche gli antichi Egizi quando nel loro equivalente del Natale addobbavano le Piramidi."
     },
     {
         "type": "text",
         "icon": "ico-omino",
-        "text": "In Giappone il Natale è festeggiato come una sorta di San Valentino durante il quale solo le coppie si scambiano i doni"
+        "text": "In Giappone il Natale è festeggiato come una sorta di San Valentino durante il quale solo le coppie si scambiano i doni."
     },
     {
         "type": "text",
         "icon": "ico-caramella",
-        "text": "È tempo di panettone, ma da dove viene questo nome? È quello di un dolce, il Pan di Toni inventato dall'aiuto cuoco della famiglia Sforza"
+        "text": "È tempo di panettone, ma da dove viene questo nome? È quello di un dolce, il Pan di Toni inventato dall'aiuto cuoco della famiglia Sforza."
     },
     {
         "type": "text",
         "icon": "ico-calza",
-        "text": "Ma Babbo Natale abita in Puglia? Il nostro barbuto personaggio si ispira a San Nicola di Myra, un vescovo le cui spoglie sono custodite a Bari"
+        "text": "Ma Babbo Natale abita in Puglia? Il nostro barbuto personaggio si ispira a San Nicola di Myra, un vescovo le cui spoglie sono custodite a Bari."
     },
     {
         "type": "image",
@@ -117,7 +117,7 @@ const source = [
     {
         "type": "text",
         "icon": "ico-neve",
-        "text": "Se pensi di non aver fritto abbastanza durante le scorse festività, ti sproniamo dicendo che in Polonia il pranzo natalizio è di 12 portate, una per ogni apostolo "
+        "text": "Se pensi di non aver fritto abbastanza durante le scorse festività, ti sproniamo dicendo che in Polonia il pranzo natalizio è di 12 portate, una per ogni apostolo."
     },
     {
         "type": "image",
